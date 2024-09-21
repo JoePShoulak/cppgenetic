11891a4569f5abd2c368db0eefc57b107dfdbb34
Joe Shoulak <joepshoulak@me.com>
2024-09-21 18:07:02 -0500

    working example of necessary functions of life forms

--- main.cpp
+++ main.cpp
@@ -11,12 +11,36 @@ public:
   {
     return 1;
   }
+
+  void mutate() override
+  {
+    char swap = dna[0];
+
+    for (int i = 0; i < dna.size() - 1; i++)
+    {
+      dna[i] = dna[i + 1];
+    }
+
+    dna[dna.size() - 1] = swap;
+  }
+
+  Codebreaker breed(const Codebreaker &partner)
+  {
+    std::vector<char> newDna;
+
+    for (int i = 0; i < dna.size(); i++)
+    {
+      newDna.push_back((rand() % 2 == 0 ? dna : partner.dna)[i]);
+    }
+
+    return Codebreaker(newDna);
+  }
 };
 
 int main(int argc, char **argv)
 {
   Codebreaker alice({'a', 'b', 'c'});
-  Lifeform<char> bob({'a', 'b', 'c'});
+  Codebreaker bob({'d', 'e', 'f'});
 
   std::cout << std::endl;
   std::cout << "DNA: " << std::endl;
@@ -26,6 +50,15 @@ int main(int argc, char **argv)
 
   std::cout << std::endl;
   std::cout << "Evaluation: " << alice.evaluate() << std::endl;
+
+  std::cout << "Breeding!" << std::endl;
+  auto charlie = alice.breed(bob);
+
+  std::cout << std::endl;
+  std::cout << "DNA: " << std::endl;
+
+  for (auto c : charlie.dna)
+    std::cout << "  " << c << std::endl;
 
   return 0;
 }
