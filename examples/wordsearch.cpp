0379f73207e68794e6cde6091cc23074004d2ae6
Joe Shoulak <joepshoulak@me.com>
2024-09-22 13:06:50 -0500

    comment and polish

--- examples/wordsearch.cpp
+++ examples/wordsearch.cpp
@@ -1,27 +1,25 @@
-#include <vector>
-#include <iostream>
-#include <cmath>
-
 #include "../genetic.h"
 
-using namespace std;
-
-#define POPULATION 10
-#define MUTATION_RATE 0.1
+// ===== SETTINGS AND PRE-DEFINITIONS =====
+#define POPULATION 1000
+#define MUTATION_RATE 1.0 / POPULATION
 #define BIAS 1
 
-string genePool = "abcdefghijklmnopqrstuvwxyz";
-string solution = "genetic";
-int dnaLength = solution.size();
+std::string genePool = "abcdefghijklmnopqrstuvwxyz"; // All possible genes
+std::string solution = "genetic";                    // The word we're searching for
+int dnaLength = solution.size();                     // DNA length must be solution length
 
+// ====== MINOR HELPER FUNCTIONS ======
+// A method for getting one random member of the gene pool
 char randomGene()
 {
   return genePool[rand() % genePool.length()];
 }
 
-vector<char> randomDNA()
+// A method for constructing an entire random set of genes
+std::vector<char> randomDNA()
 {
-  vector<char> dna;
+  std::vector<char> dna;
 
   while (dna.size() < dnaLength)
     dna.push_back(randomGene());
@@ -29,12 +27,16 @@ vector<char> randomDNA()
   return dna;
 }
 
+// ====== LIFEFORM SUBCLASS DEFINITION ======
 class Wordsearcher : public Lifeform<char>
 {
 public:
+  // Default constructor for the initial population will be random DNA
   Wordsearcher() : Lifeform<char>({randomDNA()}) {};
-  Wordsearcher(const vector<char> &dna) : Lifeform<char>(dna) {};
+  // We must be able to create a new Wordsearcher by giving it a specific DNA
+  Wordsearcher(const std::vector<char> &dna) : Lifeform<char>(dna) {};
 
+  // We calculate the fitness by counting the correct letters in correct positions
   float fitness() override
   {
     float score = 0;
@@ -46,14 +48,16 @@ public:
     return score / dna.size();
   }
 
+  // Upon mutation, one gene gets replaced with a random gene
   void mutate() override
   {
     dna[rand() % dna.size()] = randomGene();
   }
 
+  // Breeding will randomly choose between which parent to take from for each gene
   Wordsearcher breed(const Wordsearcher &partner)
   {
-    vector<char> newDna;
+    std::vector<char> newDna;
 
     for (int i = 0; i < dna.size(); i++)
       newDna.push_back((rand() % 2 == 0 ? dna : partner.dna)[i]);
@@ -62,23 +66,28 @@ public:
   }
 };
 
+// ====== MAIN FUNCTION ======
 int main(int argc, char **argv)
 {
-  cout << endl;
-  srand(time(0));
+  std::cout << std::endl; // Throw a newline to be nice
+  srand(time(0));         // Seed our RNG
 
+  // Make our genetic class given our custom Lifeform specifications and
   Genetic<Wordsearcher> genetic(POPULATION, MUTATION_RATE, BIAS);
 
-  genetic.begin(true);
-
-  while (genetic.bestMember().fitness() < 1)
-    genetic.iterate(true);
+  genetic.begin(true);           // Initialize the first generation, true for verbose
+  genetic.iterateUntil(1, true); // Run until we reach out fitness goal, true for verbose
 
   int guesses = POPULATION * genetic.getGenCount();
   double bruteExpectation = pow(26, dnaLength);
+  float percent = guesses / bruteExpectation * 100;
 
-  cout << "Completed in " << guesses << " guesses, compared to the brute force expected " << bruteExpectation << endl
-       << endl;
+  // Summarize our results
+  std::cout << "Completed in " << guesses << " guesses, "
+            << "compared to the brute force expected " << bruteExpectation << " guesses "
+            << "(" << percent << "% of the expected guesses)."
+            << std::endl
+            << std::endl;
 
   return 0;
 }
--- genetic.h
+++ genetic.h
@@ -4,23 +4,22 @@
 #include <vector>
 #include <limits>
 #include <cmath>
+#include <iostream>
 
-using namespace std;
-
-// Lifeform class, used as the members of the genetic class.
-// One must make a child class off this with definitions for the methods so it can function.
+// ===== LIFEFORM TEMPLATE CLASS =====
+// One must make a child class of this with definitions for the methods so it can function.
 template <typename T>
 class Lifeform
 {
 public:
-  vector<T> dna;
+  std::vector<T> dna;
 
-  Lifeform<T>(const vector<T> &dna) : dna(dna){};
+  Lifeform<T>(const std::vector<T> &dna) : dna(dna){};
 
   // score the lifeform
   virtual float fitness()
   {
-    return -numeric_limits<float>::max(); // This is not the floating point error
+    return -std::numeric_limits<float>::max();
   }
 
   // combine the DNA in some way
@@ -33,22 +32,23 @@ public:
   virtual void mutate() {}
 };
 
-// Genetic class handles the lifeforms and is the iterator class
+// ===== GENETIC CLASS =====
 // TODO: Restrict to Lifeforms only
 template <typename T>
-// template <typename U, typename T, typename = std::enable_if_t<std::is_base_of<Lifeform<U>, T>::value>>
 class Genetic
 {
-  int populationLimit;
+  T bestMember;
+  int popLimit;
   float mutationRate;
   int genCount = 1;
   float bias = 1; // 0 means no bias (everyone chosen evenly)
                   // 1 results in a basic weighted sum
-                  // (0, 1) results in a smaller bias
-                  // (1, inf) results in a larger bias
+                  // (0, 1) results in a smaller bias (for fitnesses >1, inverted otherwise)
+                  // (1, inf) results in a larger bias (for fitnesses >1, inverted otherwise)
 
-  vector<T> population;
+  std::vector<T> population;
 
+  // Select a member with bias for fitness from the population
   T selectMember()
   {
     float totalFitness;
@@ -57,7 +57,9 @@ class Genetic
       totalFitness += pow(m.fitness(), bias);
 
     int i = 0;
-    int randomNum = max(rand() % max(int(totalFitness), 1), 1);
+    // totalFitness could be 0, so we have to max with 1 to avoid % 0
+    // randomNum % N>0 could be 0, which would return an invalid member, so max with 1 again
+    int randomNum = std::max(rand() % std::max(int(totalFitness), 1), 1);
 
     while (randomNum > 0)
       randomNum -= population[i++].fitness();
@@ -66,32 +68,34 @@ class Genetic
   }
 
 public:
-  Genetic(int populationLimit, float mutationRate, float bias = 1) : populationLimit(populationLimit), mutationRate(mutationRate), bias(bias) {};
+  // TODO: validate some of these numbers? They should all be either positive or non-neg
+  Genetic(int popLimit, float mutationRate, float bias = 1) : popLimit(popLimit),
+                                                              mutationRate(mutationRate),
+                                                              bias(bias) {};
 
+  // Accessor because this should technically be private
   int getGenCount()
   {
     return genCount;
   }
 
+  // Initialize the population, verbose flag for output
   void begin(bool verbose = false)
   {
-    while (population.size() < populationLimit)
-    {
-      T newMember;
-      population.push_back(newMember);
-    }
+    while (population.size() < popLimit)
+      population.push_back(T());
 
     if (verbose)
       display();
   }
 
+  // Create the next generation by progenation and mutation
   void iterate(bool verbose = false)
   {
     // Progenate
-    cout << "Progenate called" << endl;
-    vector<T> newPopulation = {};
+    std::vector<T> newPopulation = {};
 
-    while (newPopulation.size() < populationLimit)
+    while (newPopulation.size() < popLimit)
     {
       auto parentA = selectMember();
       auto parentB = selectMember();
@@ -102,11 +106,18 @@ public:
 
     population = newPopulation;
 
-    // Mutate
-    cout << "Mutate called" << endl;
-    for (T &m : population)
+    for (int i = 0; i < population.size(); i++)
+    {
+      // Mutate
       if (rand() % 100 / 100 < mutationRate)
-        m.mutate();
+        population[i].mutate();
+
+      // Find the best member while we're looping over everything
+      if (i == 0)
+        bestMember = population[i];
+      else if (population[i].fitness() > bestMember.fitness())
+        bestMember = population[i];
+    }
 
     // Cleanup
     genCount++;
@@ -115,28 +126,25 @@ public:
       display();
   }
 
-  T bestMember()
+  // Call iterate() until our bestMember has a fitness at or above our goal
+  void iterateUntil(float fitnessGoal, bool verbose = false)
   {
-    T best = population[0];
-
-    for (int i = 1; i < populationLimit; i++)
-      if (population[i].fitness() > best.fitness())
-        best = population[i];
-
-    return best;
+    while (bestMember.fitness() < fitnessGoal)
+      iterate(verbose);
   }
 
+  // Display a summary of the current generation
   void display()
   {
-    cout << "Gen count: " << genCount << endl;
-    cout << "Pop size: " << population.size() << endl;
-    cout << "Best fitness: " << bestMember().fitness() << endl;
-    cout << "Best DNA: ";
-    for (auto k : bestMember().dna)
-      cout << k;
+    std::cout << "Gen count: " << genCount << std::endl;
+    std::cout << "Pop size: " << population.size() << std::endl;
+    std::cout << "Best fitness: " << bestMember.fitness() << std::endl;
+    std::cout << "Best DNA: ";
+    for (auto k : bestMember.dna)
+      std::cout << k;
 
-    cout << "\n"
-         << endl;
+    std::cout << std::endl
+              << std::endl;
   }
 };
 
